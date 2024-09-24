904697c3a4184be63d62c1dada3664af231af96d
Fiifi <botchwayfiifiessirifie@gmail.com>
2024-09-23 21:46:44 -0400

    applied polynomoal regression

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import cvzone
 import math
 import cvzone
 from cvzone.ColorModule import ColorFinder
-import numpy
+import numpy as np
 
 # Initialization
 
-cam = cv2.VideoCapture('Test ball/Videos/vid (1).mp4')
+cam = cv2.VideoCapture('Test ball/Videos/vid (3).mp4')
+x_pos_list = []
+y_pos_list = []
+x_path = [i for i in range(1300)]
+# y_path = []
 
 while True:
     # Find image
@@ -23,10 +27,34 @@ while True:
     # Find ball location
     imgContours, contours = cvzone.findContours(img,mask,minArea=500)
 
+    # Trace ball path
+    if contours:
+        x_pos_list.append(contours[0]['center'][0])   
+        y_pos_list.append(contours[0]['center'][1])  
+    # Calculations y = Ax^2 + Bx + C
+    if x_pos_list:
+        for i, (x,y) in enumerate(zip(x_pos_list,y_pos_list)):
+            cv2.circle(imgContours,(x,y),5,(0,255,0),cv2.FILLED)
+            if i>0:
+                cv2.line(imgContours,(x,y),(x_pos_list[i-1],y_pos_list[i-1]),(0,255,0),2)
+        
+        # Calculations y = Ax^2 + Bx + C
+        a,b,c = np.polyfit(x_pos_list,y_pos_list,2)
+
+        for x in x_path:
+            y = int((a*x*x) + (b*x) + c)
+            cv2.circle(imgContours,(x,y),1,(0,0,255),cv2.FILLED)
+
+
+
+
+
+
+
 
 
 
     # Display 
-    img = cv2.resize(color, (0,0),None,0.7,0.7)
+    imgContours = cv2.resize(imgContours, (0,0),None,0.7,0.7)
     cv2.imshow('Image', imgContours)
     cv2.waitKey(40)
